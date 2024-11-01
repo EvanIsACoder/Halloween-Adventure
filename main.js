24daefeb35aeb6bcaca69969f4776b5a2562cb4b
EvanIsACoder <151179467+EvanIsACoder@users.noreply.github.com>
2024-11-01 17:05:43 +0000

    clean up

--- main.js
+++ main.js
@@ -140,22 +140,3 @@ function win2() {
 
 
 main();
-
-
-
-/* ideas:
- * Player wakes up with arms tied like hes a patient in an insane asylum
- * there are doors on opposite sides of the room, one says 'North Door' the other says 'South Door'. 
- * player picks which door they want to go to.  * 
- * North Door goes to the dinning room. 
-outh Door goes to the library. 
- * (south door choice)Player pulls out book and secret staricase appears behind the shelve 
- * If user goes upstairs, they escape through the window of the master bedroom.
- * If user goes downstairs, the bookshelves close behind them and there is a ghost downstairs that catches them.
- * (North door choice) Player finds floating glasses moving around the room. 
- * User finds stack of plates at the center of table, with cutlery set up and puts the plates in the right spot.
- * Door opens to hall wit
- two rooms, user picks to go into either the Bathroom or 
-
-
-*/
